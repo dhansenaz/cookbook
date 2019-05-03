7b5281e752fba4716e502f4acf08748d3afc9823
dhansenaz <dhansen@allthingsdot.com>
2019-05-03 07:32:47 -0700

    initial commit

---
+++ server.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize(process.env.DB_URI, { dialect:'postgres'});
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
